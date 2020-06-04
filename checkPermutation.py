53f75d2adebe09bc2cd8d0b7cff70ee7e29674b8
liyaSileshi <liyasileshi@gmail.com>
2020-06-03 23:12:58 -0700

    permutation

--- checkPermutation.py
+++ checkPermutation.py
@@ -4,24 +4,42 @@
 #ask if comparision is case sensetive and whitespace is significant
 #using hashtables......
 def check(str1, str2):
-    flag = True
-    str1_index = 0
-    str1 = str1.lower() #case insensetive
-    str2 = str2.lower() #case insensetive
-    if len(str1) == len(str2): #check if the two strings have similar length
-        while str1_index < len(str1): #to not be index out of range
-            for char2 in str2:
-                if str1[str1_index] == char2:
-                    flag = True 
-                    break
-                else: 
-                    flag = False
-            if flag == True: #first index string matches second string index
-                str1_index += 1 #increment to check the next index for the loop
-            else: #if flag was false at this point, the string index didn't found a match
-                return False
-    else:
+    '''Naive method'''
+    # flag = True
+    # str1_index = 0
+    # str1 = str1.lower() #case insensetive
+    # str2 = str2.lower() #case insensetive
+    # if len(str1) == len(str2): #check if the two strings have similar length
+    #     while str1_index < len(str1): #to not be index out of range
+    #         for char2 in str2:
+    #             if str1[str1_index] == char2:
+    #                 flag = True 
+    #                 break
+    #             else: 
+    #                 flag = False
+    #         if flag == True: #first index string matches second string index
+    #             str1_index += 1 #increment to check the next index for the loop
+    #         else: #if flag was false at this point, the string index didn't found a match
+    #             return False
+    # else:
+    #     return False
+    # return flag
+
+    '''Method 2: sorting both strings '''
+    if len(str1) != len(str2):
         return False
-    return flag
+    # case insensetive
+    str1 = str1.lower()
+    str2 = str2.lower()
+
+    # sort both strings
+    str1 = list(str1)
+    str1.sort()
+    str2 = list(str2)
+    str2.sort()
+
+    if str1 == str2:
+        return True
+    return False
 if __name__ == "__main__":
     print(check('God','dog'))
